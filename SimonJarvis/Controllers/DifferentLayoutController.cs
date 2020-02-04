184e3638a6229786eb2489c8f4c91b50e15d0caf
nomissivraj <simonpeterjarvis@gmail.com>
2020-02-04 22:02:59 +0000

    Add project files.

---
+++ SimonJarvis/Controllers/DifferentLayoutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimonJarvis.Controllers
+{
+    public class DifferentLayoutController : Controller
+    {
+        // GET: DifferentLayout
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SimonJarvis/Controllers/KnowledgeSubController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimonJarvis.Models;
+
+namespace SimonJarvis.Controllers
+{
+    
+    [RoutePrefix("Knowledge/KnowledgeSub")]
+    public class KnowledgeSubController : Controller
+    {
+        // GET: KnowledgeSub
+        [Route]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("One")]
+        public ActionResult One(int id = 0)
+        {
+            TestModel test = new TestModel();
+
+            if (id != 0)
+            {
+                test.id = id;
+                return View("One", test);
+            } else
+            {
+                return View("One");
+            }
+
+            
+        }
+
+        [HttpGet]
+        [Route("Two")]
+        public ActionResult Two()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Three")]
+        public ActionResult Three()
+        {
+            return View();
+        }
+    }
+}
