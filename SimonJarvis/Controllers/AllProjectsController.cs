c49a15b8e361c953b91a01deeaef8289c01ce840
nomissivraj <simonpeterjarvis@gmail.com>
2020-02-15 23:40:48 +0000

    Sorting/filtering and pagination all in one view area/controller

--- SimonJarvis/Controllers/AllProjectsController.cs
+++ SimonJarvis/Controllers/AllProjectsController.cs
@@ -16,6 +16,74 @@ namespace SimonJarvis.Controllers
 
             return View();
         }
-        
+
+        // GET: ProjectCards
+        [HttpGet]
+        public PartialViewResult ProjectCards(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+
+
+            //STUFF
+            List<ProjectInfoCard> projects = new List<ProjectInfoCard>();
+
+            ProjectInfoCard pIC = new ProjectInfoCard();
+            pIC.id = 1;
+            pIC.Name = "One";
+            pIC.status = "active";
+            projects.Add(pIC);
+
+            pIC = new ProjectInfoCard();
+            pIC.id = 2;
+            pIC.Name = "Two";
+            pIC.status = "active";
+            projects.Add(pIC);
+
+            pIC = new ProjectInfoCard();
+            pIC.id = 3;
+            pIC.Name = "Three";
+            pIC.status = "inactive";
+            projects.Add(pIC);
+
+            pIC = new ProjectInfoCard();
+            pIC.id = 4;
+            pIC.Name = "Four";
+            pIC.status = "active";
+            projects.Add(pIC);
+
+            //STUFF END
+            switch (sortOrder)
+            {
+                case "Name":
+                    projects = projects.OrderBy(x => x.Name).ToList();
+                    break;
+                case "Id":
+                    projects = projects.OrderByDescending(x => x.id).ToList();
+                    break;
+                case "Status":
+                    projects = projects.OrderBy(x => x.status).ToList();
+                    break;
+                default:
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return PartialView("_ProjectCards", projects.ToPagedList(pageNumber, pageSize));
+        }
     }
 }
