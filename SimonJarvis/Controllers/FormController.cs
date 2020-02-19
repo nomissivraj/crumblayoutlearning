0a593f32e3970b09fbaaf689b0f602325c4fa496
nomissivraj <simonpeterjarvis@gmail.com>
2020-02-19 21:53:36 +0000

    Form Testing for dateTime

---
+++ SimonJarvis/Controllers/FormController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimonJarvis.Models.ViewModels;
+
+namespace SimonJarvis.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index(FormViewModel model)
+        {
+
+            FormViewModel m = new FormViewModel();
+            m.Name = model.Name;
+            m.Date = model.Date;
+
+            return View();
+        }
+    }
+}
---
+++ SimonJarvis/Models/ViewModels/FormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimonJarvis.Models.ViewModels
+{
+    public class FormViewModel
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
